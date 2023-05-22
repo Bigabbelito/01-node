c65850785867f96fc17a8f0082b019edbdeade78
Abbe <abdullah.abou.shihab@icloud.com>
2023-05-22 14:22:07 +0200

    add a question

--- start.js
+++ start.js
@@ -1,13 +1,20 @@
-console.log('Hello World')
+import { question } from "readline-sync"
 
-var readlineSync = require('readline-sync');
+
+console.log('Welcome User!')
+let name = question('What is your name? ')
+
+console.log(`Welcome again, ${name}! `)
+
+// var readlineSync = require('readline-sync');
  
-// Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+// // Wait for user's response.
+// var userName = readlineSync.question('May I have your name? ');
+// console.log('Hi ' + userName + '!');
  
-// Handle the secret text (e.g. password).
-var favFood = readlineSync.question('What is your favorite food? ', {
-  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
-});
-console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+// // Handle the secret text (e.g. password).
+// var favFood = readlineSync.question('What is your favorite food? ', {
+//   hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+// });
+// console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+
